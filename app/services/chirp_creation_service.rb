74c9a36c3afe9f8ee0b6447c831592f5b68db471
Che Nxusani <shayarealg@gmail.com>
2013-02-09 11:06:21 +0000

    sdf

--- app/services/chirp_creation_service.rb
+++ app/services/chirp_creation_service.rb
@@ -3,9 +3,17 @@ class ChirpCreationService
     user_name = params.delete(:user_name)
     user = User.find_or_create_by_name!(user_name)
     Chirp.create(params.merge(:user_id => user.id))
+
   end
 
   def create!(params)
+    cw ||= AWS::CloudWatch.new
+    cw.put_metric_data(
+      :namespace => 'chirps',
+      :metric_data => [
+        { :metric_name => 'chirp', :value => 1 }
+      ]
+    )
     chirp = create(params)
     chirp.save!
     chirp
